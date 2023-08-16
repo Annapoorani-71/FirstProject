6a6f6b5da5a0a548f208e86dcb4b1ef9facc8d13
Annapoorani-71 <annapooranikumar71@gmail.com>
2023-08-16 14:44:33 +0530

    first1 commit

---
+++ Welcome.java
@@ -0,0 +1,12 @@
+
+class Welcome
+{
+public static void main(String arr[])
+{
+
+int n1=Integer.parseInt(arr[0]);
+int n2=Integer.parseInt(arr[1]);
+System.out.println(n1);
+System.out.println(n2);
+}
+}
